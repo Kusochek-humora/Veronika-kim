4796b74e8bf38d7c2c4975ccf05c43cb46105055
Kusochek-humora <69989303+Kusochek-humora@users.noreply.github.com>
2023-10-27 20:20:04 +0600

    fix bliad

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
         closeMenubtn = document.getElementById('close-menu'),
         mobileMenu = document.getElementById('mobile-menu'),
         overlay = document.querySelector('.overlay');
+
     function openMenuHandler(e) {
         mobileMenu.classList.add('active');
         overlay.classList.add('active');
@@ -15,4 +16,25 @@ document.addEventListener('DOMContentLoaded', function () {
     overlay.addEventListener('click', closeMenuHandler);
     openMenubtn.addEventListener('click', openMenuHandler);
     closeMenubtn.addEventListener('click', closeMenuHandler);
+
+
+
+    const promoBg = document.getElementById('promo__bg'),
+        promoSection = document.getElementById('promo');
+    window.addEventListener('resize', function () {
+        promoSection.setAttribute('style', `height:${promoBg.scrollHeight}px`);
+    })
+    // let options = {
+    //     root: promoSection,
+    //     rootMargin: "0px",
+    //     threshold: 1.0,
+    // };
+    // let callback = function (entries, observer) {
+
+    //     console.log('qweqwe')
+
+    // };
+    // let observer = new IntersectionObserver(callback, options);
+    // observer.observe(promoBg);
+
 })
